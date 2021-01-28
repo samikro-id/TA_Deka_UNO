e7ad646fde06c0a31013a20714fb7d430072c3a4
Santinilestari Energi Indonesia <it.santinilestari@gmail.com>
2021-01-28 13:14:46 +0700

    add serial

--- TA_Deka_UNO.ino
+++ TA_Deka_UNO.ino
@@ -1,5 +1,4 @@
-#include <ESP8266_Lib.h>
-#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
 
 #define TEGANGAN_PIN    A0
 #define ARUS_PIN        A1
@@ -7,63 +6,129 @@
 #define RELAY1_PIN      8
 #define RELAY2_PIN      9
 
-#define WIFI_RX         2
-#define WIFI_TX         3
+uint32_t led_time;
+bool led_state          = false;
 
-#define ESP826_BAUD     115200
+float volt;
+double arus;
+int sensitivitas        = 185; //tegantung sensor arus yang digunakan, yang ini 5A
+int teganganoffset      = 2.5;
+float teganganbawah     = 10;
+float teganganatas      = 13;
 
-char ssid[] = "samikro";
-char pass[] = "samikroid";
-
-SoftwareSerial EspSerial(WIFI_RX, WIFI_TX);
-ESP8266 wifi(&EspSerial);
-
-float Volt;
-double Arus;
-int sensitivitas = 185; //tegantung sensor arus yang digunakan, yang ini 5A
-int teganganoffset = 2.5;
-float teganganbawah = 10;
-float teganganatas = 13;
+uint16_t energy = 0;
 
 uint8_t persen;
+bool relay_state        = false;
+
 uint32_t previous_time;
 
+#define SERIAL_BAUD     115200
+#define SERIAL_LEN      1000
+String serial_buff;
+bool serial_complete    = false;
+
 void setup(){
-    Serial.begin(115200);               Serial.println("start");
-    pinMode(RELAY1_PIN, OUTPUT);
-    pinMode(RELAY2_PIN, OUTPUT);
+  delay(1000);
+  Serial.begin(SERIAL_BAUD);               
+  initIo();
 
-    digitalWrite(RELAY1_PIN, HIGH);
-    digitalWrite(RELAY2_PIN, HIGH);
-    delay(100);
-
-    EspSerial.begin(ESP826_BAUD);
-
-    previous_time = millis();
+  serial_buff.reserve(SERIAL_LEN);
+  
+  previous_time = millis();
+  led_time = millis();
 }
 
 void loop(){
-    if((millis() - previous_time) > 200){
-        bacaSensor();
-        previous_time = millis();
-    }
+  if((millis() - previous_time) > 200){
+    bacaSensor();
+    previous_time = millis();
+  }
+
+  if((millis() - led_time) > 200){
+    toggleLed();
+    led_time = millis();
+  }
+
+  if(serial_complete){
+    serial_complete = false;
+    prosesData();
+    serial_buff = "";
+  }
 }
 
 void bacaSensor(){
-    /* BACA TEGANGAN */
-    uint16_t VoltRaw = analogRead(TEGANGAN_PIN);      
-    float pinVolt = (float) (5 * VoltRaw) / 1023;   
-    Volt = (float) pinVolt * 5;                   Serial.print("BAT : ");     Serial.println(Volt);
+  /* BACA TEGANGAN */
+  uint16_t voltRaw = analogRead(TEGANGAN_PIN);      
+  float pinvolt = (float) (5 * voltRaw) / 1023;   
+  volt = (float) pinvolt * 5;
 
-    /* BACA ARUS */
-    uint16_t ArusRaw = analogRead(ARUS_PIN);      //Serial.print("RAW : ");     Serial.println(ArusRaw); 
-    double pinArus = 5000 * (ArusRaw / 1024.0);        //Serial.print("PIN : ");     Serial.println(pinArus);
-    Arus = (float) ((pinArus - teganganoffset) / sensitivitas); Serial.print("ARU : ");     Serial.println(Arus);
+  /* BACA arus */
+  uint16_t arusRaw = analogRead(ARUS_PIN);  
+  double pinArus = 5000 * (arusRaw / 1024.0);     
+  arus = (float) ((pinArus - teganganoffset) / sensitivitas);
 
-    if(Volt > teganganbawah && Volt < teganganatas ){
-      persen = ((Volt - teganganbawah) / (teganganatas - teganganbawah)) * 100; Serial.println(persen);
+  if(volt > teganganbawah && volt < teganganatas ){
+    persen = ((volt - teganganbawah) / (teganganatas - teganganbawah)) * 100;
+  }
+  else{
+    persen = 0;
+  }
+}
+
+void prosesData(){
+  StaticJsonBuffer<SERIAL_LEN> JSONBuffer;
+
+  JsonObject& root = JSONBuffer.parseObject(serial_buff);
+  if(root.success()){
+    if(root["op"] == "data"){
+      serial_buff = "{\"tegangan\":" + String(volt, 1) +",\"arus\":"+ String(arus, 2) + ",\"energy\":" + String(energy, DEC) + ",\"on\":" + String(relay_state, DEC) + "}";
     }
-    else{
-      persen = 0;
+  }
+}
+
+/******* Serial Interrupt Event Callback ********/
+void serialEvent(){
+  while(Serial.available()){
+    char inChar = (char) Serial.read();
+    if(inChar == '\n'){
+      serial_complete = true;
+    }else if(inChar == '\r'){
+      // do nothing
+    }else{
+      serial_buff += inChar;
     }
+  }
+}
+
+/****** LED function ******/
+void ledState(bool state){
+  if(state){  digitalWrite(LED_BUILTIN, HIGH);  }
+  else{       digitalWrite(LED_BUILTIN, LOW);   }
+  led_state = state;
+}
+
+void toggleLed(){
+  if(led_state){
+    digitalWrite(LED_BUILTIN, LOW);
+    led_state = false;
+  }else{
+    digitalWrite(LED_BUILTIN, HIGH);
+    led_state = true;
+  }
+}
+
+/****** Initialize *******/
+
+void initIo(){
+  pinMode(RELAY1_PIN, OUTPUT);
+  digitalWrite(RELAY1_PIN, HIGH);
+
+  pinMode(RELAY2_PIN, OUTPUT);
+  digitalWrite(RELAY2_PIN, HIGH);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  led_state = false;
+
 }
